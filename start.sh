d02d79afb188df2dc6fed2164106633145034f2b
amazingclaude <liaowen1992@gmail.com>
2026-04-28 09:25:15 +0100

    modify the start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
-pip install -r /home/site/wwwroot/requirements.txt
+cd /home/site/wwwroot
+python -m venv antenv
+source antenv/bin/activate
+pip install -r requirements.txt
 gunicorn -k uvicorn.workers.UvicornWorker app:app --bind=0.0.0.0:$PORT
